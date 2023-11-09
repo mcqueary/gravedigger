1e8b90b520ce665adda6f6a35a21bf8dd56b3e07
mcqueary <larry@mcqueary.org>
2023-11-08 21:43:47 -0700

    Incremental

--- graver/models.py
+++ graver/models.py
@@ -2,19 +2,42 @@ import os
 import sqlite3
 
 from pydantic import BaseModel
+from .soup import (
+    get_birth_date,
+    get_birth_place,
+    get_burial_plot,
+    get_death_date,
+    get_death_place,
+    get_name,
+    get_soup,
+)
 
 
 class Grave(BaseModel):
-    id: int
-    url: str
-    name: str
-    birth: str
-    birthplace: str
-    death: str
-    deathplace: str
-    burial: str
-    plot: str
-    more_info: bool
+    _id: int
+    _url: str
+    _name: str
+    _birth: str
+    _birthplace: str
+    _death: str
+    _deathplace: str
+    _burial: str
+    _plot: str
+    _more_info: bool
+
+    @classmethod
+    def instance_from_soup(id, tree):
+        url = "https://www.findagrave.com/memorial/" + str(id)
+        name = get_name(tree)
+        birth = get_birth_date(tree)
+        birthplace = get_birth_place(tree)
+        death = get_death_date(tree)
+        deathplace = get_death_place(tree)
+        plot = get_burial_plot(tree)
+        more_info = False
+        return Grave(
+            id, url, name, birth, birthplace, death, deathplace, plot, more_info
+        )
 
     @classmethod
     def create_table(cls, database_name="graver.db"):
@@ -27,22 +50,34 @@ class Grave(BaseModel):
         )
         conn.close()
 
+    def __init__(self, url):
+        self._url = url
+        tree = get_soup(url)
+        self._name = get_name(tree)
+        self._birth = get_birth_date(tree)
+        self._birthplace = get_birth_place(tree)
+        self._death = get_death_date(tree)
+        self._deathplace = get_death_place(tree)
+        self._plot = get_burial_plot(tree)
+        self._more_info = False
+        return self
+
     def save(self) -> "Grave":
         with sqlite3.connect(os.getenv("DATABASE_NAME", "graves.db")) as con:
             con.cursor().execute(
                 "INSERT INTO graves (id,author,title,content) VALUES"
                 + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 (
-                    self.id,
-                    self.url,
-                    self.name,
-                    self.birth,
-                    self.birthplace,
-                    self.death,
-                    self.deathplace,
-                    self.burial,
-                    self.plot,
-                    self.more_info,
+                    self._id,
+                    self._url,
+                    self._name,
+                    self._birth,
+                    self._birthplace,
+                    self._death,
+                    self._deathplace,
+                    self._burial,
+                    self._plot,
+                    self._more_info,
                 ),
             )
             con.commit()

3ff309074b27ae85fbafb805b856c4ec7f5ec8ae
Larry McQueary <larry@mcqueary.org>
2023-11-20 11:41:59 -0700

    test: Refactor tests and class models for better testability and coverage

---
+++ tests/test_cemetery.py
@@ -0,0 +1,86 @@
+from urllib.error import HTTPError
+
+import pytest
+
+from definitions import ROOT_DIR
+from graver.cemetery import Cemetery, Driver
+
+mixed_urls = [
+    "https://www.findagrave.com/cemetery/53514",
+    "https://www.findagrave.com/memorial/12345",
+    "https://www.findagrave.com/cemetery/55276/memorial-search",
+]
+
+cemetery_urls = [
+    "https://www.findagrave.com/cemetery/55276",
+    "https://www.findagrave.com/cemetery/153/",
+]
+
+list_urls = [
+    "https://www.findagrave.com/cemetery/55276/memorial-search",
+    "https://www.findagrave.com/cemetery/153/memorial-search?",
+]
+
+cem_3136_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/cem-3136.html")
+
+cem_fake: dict = {
+    "id": 41404,
+    "findagrave_url": "https://www.findagrave.com/memorial/1784/grace-brewster-hopper",
+    "name": "My Fake Cemetery",
+    "location": "Dallas, Dallas, Texas, USA",
+    "coords": "38.8775405, -77.0654917",
+}
+
+
+@pytest.mark.parametrize("expected", [cem_fake])
+def test_cemetery_from_dict(expected: dict):
+    cem = Cemetery.from_dict(expected)
+    assert cem.id == expected["id"]
+    assert cem.findagrave_url == expected["findagrave_url"]
+    assert cem.name == expected["name"]
+    assert cem.location == expected["location"]
+    assert cem.coords == expected["coords"]
+
+
+@pytest.mark.parametrize("expected", [cem_fake])
+def test_cemetery_to_dict(expected: dict):
+    c = Cemetery.from_dict(expected)
+    result = c.to_dict()
+    assert result["id"] == expected["id"]
+    assert result["findagrave_url"] == expected["findagrave_url"]
+    assert result["name"] == expected["name"]
+    assert result["location"] == expected["location"]
+    assert result["coords"] == expected["coords"]
+
+
+@pytest.mark.parametrize("uri", [cem_3136_uri])
+def test_cemetery(uri):
+    cem = Cemetery(uri)
+    cem2 = Cemetery(uri)
+    assert cem == cem2
+    cem3 = Cemetery(uri, get=False, scrape=False)
+    cem3.id = 41405
+    # test for class inequality
+    assert cem3 != 42
+    # test for instance inequality
+    assert cem3 != cem2
+
+    assert cem.id == 3136
+    assert cem.name == "Crown Hill Memorial Park"
+    assert (
+        cem.findagrave_url
+        == "https://www.findagrave.com/cemetery/3136/crown-hill-memorial-park"
+    )
+    assert cem.location == "Dallas, Dallas County, Texas, USA"
+    assert cem.coords == "32.86780,-96.86220"
+
+
+@pytest.mark.integration_test
+@pytest.mark.parametrize(
+    "findagrave_url", ["https://www.findagrave.com/should-produce-404"]
+)
+def test_memorial_driver_raises_http_error(findagrave_url):
+    with pytest.raises(Exception) as e:
+        Driver.get(findagrave_url)
+    assert e.errisinstance(HTTPError)
+    assert e.value.code == 404
---
+++ tests/test_cli.py
@@ -0,0 +1,89 @@
+import importlib.metadata
+import os
+
+import pytest
+
+import graver
+import graver.cli as cli
+from definitions import ROOT_DIR
+from graver.constants import APP_NAME
+from graver.memorial import Memorial
+
+asimov_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/asimov.html")
+shoulders_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/shoulders.html")
+merged_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/merged.html")
+maiden_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/dolores-maiden.html")
+cem_3136_uri = pytest.helpers.to_uri(ROOT_DIR + "/tests/data/cem-3136.html")
+
+
+@pytest.mark.parametrize("arg", ["-v", "--version"])
+def test_version_multiple_ways(helpers, arg):
+    assert helpers.graver_cli(arg) == "{} v{}".format(APP_NAME, graver.__version__)
+    metadata = importlib.metadata.metadata("graver")
+    name_str = metadata["Name"]
+    version_str = metadata["Version"]
+    expected_str = "{} v{}".format(name_str, version_str)
+    result = helpers.graver_cli(arg)
+    assert expected_str in result
+
+
+@pytest.mark.parametrize(
+    "mem_id",
+    [
+        10325,
+    ],
+)
+def test_cli_scrape(mem_id, helpers):
+    url_file = os.getenv("MULTI_LINE_UNIT_TEST_FILE")
+    db = os.getenv("DATABASE_NAME")
+    command = "scrape {} --db {}".format(url_file, db)
+    output = helpers.graver_cli(command)
+    assert "Successfully scraped" in output
+    m = Memorial.get_by_id(mem_id)
+    assert m is not None
+    assert m.id == mem_id
+
+
+@pytest.mark.parametrize(
+    "expected_id, url",
+    [
+        (1075, "https://secure.findagrave.com/cgi-bin/fg.cgi?page=gr&GRid=1075"),
+        (544, "https://www.findagrave.com/memorial/544"),
+    ],
+)
+def test_get_id_from_url(expected_id: int, url: str):
+    id = cli.get_id_from_url(url)
+    assert id == expected_id
+
+
+@pytest.mark.parametrize("urls", [[""], ["foo", "bar", "baz"]])
+def test_print_failed_urls(urls):
+    cli.print_failed_urls(urls)
+
+
+def test_cli_scrape_with_bad_url_file(helpers):
+    url_file = os.getenv("BAD_DATA_FILENAME")
+    db = os.getenv("DATABASE_NAME")
+    command = "scrape {} --db {}".format(url_file, db)
+    output = helpers.graver_cli(command)
+    assert "Failed urls were:\nhttps://www.findagrave.com/this-does-not-exist" in output
+
+
+live_ids = (1075, 534, 574, 627, 544, 6, 7376621, 95929698, 1347)
+
+
+@pytest.mark.integration_test
+@pytest.mark.parametrize(
+    "mem_id",
+    [
+        1075,
+    ],
+)
+def test_cli_scrape_with_single_url_file(mem_id, helpers):
+    url_file = os.getenv("SINGLE_LINE_FILENAME")
+    db = os.getenv("DATABASE_NAME")
+    command = "scrape {} --db {}".format(url_file, db)
+    output = helpers.graver_cli(command)
+    print(output)
+    m = Memorial.get_by_id(mem_id)
+    assert m.id == mem_id
